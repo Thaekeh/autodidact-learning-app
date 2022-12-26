b79d065137fe8322b277199d0c3fa1349a8527d1
Thaeke Hekkenberg <thaekeh@gmail.com>
2022-12-26 20:44:01 +0100

    Call signIn from LoginForm

--- main-app/components/authentication/LoginForm.tsx
+++ main-app/components/authentication/LoginForm.tsx
@@ -1,11 +1,28 @@
-import { Form, Input } from "antd";
+import { Button, Form, Input } from "antd";
 import React from "react";
+import { signIn } from "next-auth/react";
 
 export const LoginForm = () => {
+  const onFinish = async (values: any) => {
+    const result = await signIn("credentials", {
+      redirect: false,
+      email: values.email,
+      password: values.password,
+    });
+  };
+
   return (
-    <Form>
-      <Form.Item required={true} label="Email">
+    <Form onFinish={onFinish} wrapperCol={{ span: 6 }} layout="horizontal">
+      <Form.Item required={true} label={"Email"} name={"email"}>
         <Input type="email"></Input>
+      </Form.Item>
+      <Form.Item required={true} label={"Password"} name={"password"}>
+        <Input type="password"></Input>
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Login
+        </Button>
       </Form.Item>
     </Form>
   );
