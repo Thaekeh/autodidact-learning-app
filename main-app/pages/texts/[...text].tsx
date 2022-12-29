e21dcaed0669638a38fec59e350d8aa8e292ac4f
Thaeke Hekkenberg <thaekeh@gmail.com>
2022-12-29 22:07:55 +0100

    Move text editor to [...text] route

---
+++ main-app/pages/texts/[...text].tsx
@@ -0,0 +1,105 @@
+import {
+  Button,
+  Card,
+  Container,
+  Grid,
+  Row,
+  Textarea,
+  Text,
+} from "@nextui-org/react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { Edit2 } from "react-feather";
+import styled from "@emotion/styled";
+
+export default function TextPage() {
+  const [isInEditMode, setIsInEditMode] = useState(false);
+  const [text, setText] = useState<string>(
+    "This is some testing text with a longer content"
+  );
+
+  const { query } = useRouter();
+  const textId = query.text && query.text[0];
+
+  const handleTextClick = (event: React.MouseEvent<HTMLSpanElement>) => {
+    console.log(event?.currentTarget.innerHTML);
+  };
+
+  const mappedText = () => {
+    const textInArray = text.split(" ");
+    return textInArray.map((word) => {
+      const randomNumber = Math.floor(Math.random() * 100000);
+      return (
+        <React.Fragment key={`${word}-${randomNumber}`}>
+          <HoverableWord onClick={handleTextClick}>{word}</HoverableWord>
+          &nbsp;
+        </React.Fragment>
+      );
+    });
+  };
+
+  return (
+    <Grid.Container
+      gap={2}
+      justify="center"
+      css={{ marginTop: `2rem`, width: `100vw`, margin: 0 }}
+    >
+      <Grid
+        xs
+        css={{
+          maxHeight: `80vh`,
+          height: `80vh`,
+        }}
+      >
+        <Card>
+          <Card.Body>
+            {isInEditMode ? (
+              <>
+                <Text h4>Edit your text</Text>
+                <Textarea
+                  animated={false}
+                  value={text}
+                  onChange={(event) => setText(event.target.value)}
+                  maxRows={50}
+                ></Textarea>
+              </>
+            ) : (
+              <>
+                <Text h4>Click on any word</Text>
+                <Container wrap="wrap" css={{ maxWidth: `100%` }}>
+                  <Text css={{ display: `flex`, flexWrap: `wrap` }}>
+                    {mappedText()}
+                  </Text>
+                </Container>
+              </>
+            )}
+          </Card.Body>
+          <Card.Divider />
+          <Card.Footer>
+            <Row justify="flex-end">
+              <Button size="xs" light>
+                Cancel
+              </Button>
+              <Button icon size="sm" rounded>
+                <Edit2 onClick={() => setIsInEditMode(!isInEditMode)}></Edit2>
+              </Button>
+            </Row>
+          </Card.Footer>
+        </Card>
+      </Grid>
+      <Grid xs>
+        <h1>Test</h1>
+      </Grid>
+    </Grid.Container>
+  );
+}
+
+const HoverableWord = styled.span`
+  padding: 1px;
+  box-sizing: border-box;
+  :hover {
+    background-color: #8686ff;
+    cursor: pointer;
+    border-radius: 3px;
+  }
+`;
