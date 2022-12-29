4f105805fd5594712c0664d5c266ce33a1d93802
Thaeke Hekkenberg <thaekeh@gmail.com>
2022-12-29 22:06:17 +0100

    Add avatar icon to navbar

--- main-app/components/Navbar.tsx
+++ main-app/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navbar, Avatar } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
 import NextLink from "next/link";
+import { User } from "react-feather";
 
 export const ComposedNavbar = () => {
   const { data } = useSession();
@@ -23,7 +24,12 @@ export const ComposedNavbar = () => {
         {data?.user && (
           <Navbar.Link as={"span"}>
             <NextLink href="/profile">
-              {data.user && <Avatar text={data.user.name || ""} />}
+              {data.user && (
+                <Avatar
+                  text={data.user.name || ""}
+                  icon={data.user.image ? undefined : <User />}
+                />
+              )}
             </NextLink>
           </Navbar.Link>
         )}
