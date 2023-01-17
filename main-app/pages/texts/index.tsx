80ab06302f7a7099166575e760c21887851aa2de
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-01-17 23:05:28 +0100

    Update Texts page

--- main-app/pages/texts/index.tsx
+++ main-app/pages/texts/index.tsx
@@ -1,25 +1,61 @@
-import { Button, Container, Table } from "@nextui-org/react";
+import {
+  Button,
+  Col,
+  Container,
+  red,
+  Row,
+  Table,
+  Tooltip,
+} from "@nextui-org/react";
 import { getToken } from "next-auth/jwt";
 import { getSession, useSession } from "next-auth/react";
 import { useState } from "react";
+import { Edit, Edit2, Eye, Trash } from "react-feather";
+import { IconButton } from "../../components/buttons/IconButton";
 import clientPromise from "../../lib/mongodb";
+import { Text } from "./types";
 
-export default function Texts({ texts }) {
+export default function Texts({ texts }: { texts: Text[] }) {
   return (
     <Container>
       <Table>
         <Table.Header>
           <Table.Column>Name</Table.Column>
           <Table.Column>Last opened</Table.Column>
-          <Table.Column>f</Table.Column>
+          <Table.Column> </Table.Column>
         </Table.Header>
         <Table.Body>
           {texts.map((text) => {
             return (
-              <Table.Row key={text.name}>
+              <Table.Row key={text.id}>
                 <Table.Cell>{text.name}</Table.Cell>
-                <Table.Cell>{text.lastOpened}</Table.Cell>
-                <Table.Cell> </Table.Cell>
+                <Table.Cell>{text.updatedAt}</Table.Cell>
+
+                <Table.Cell>
+                  <Row justify="center" align="center">
+                    <Col css={{ d: "flex" }}>
+                      <Tooltip content="Open text">
+                        <IconButton>
+                          <Eye />
+                        </IconButton>
+                      </Tooltip>
+                    </Col>
+                    <Col css={{ d: "flex" }}>
+                      <Tooltip content="Rename text">
+                        <IconButton>
+                          <Edit2 />
+                        </IconButton>
+                      </Tooltip>
+                    </Col>
+                    <Col css={{ d: "flex" }}>
+                      <Tooltip content="Delete text">
+                        <IconButton>
+                          <Trash color={red.red600} />
+                        </IconButton>
+                      </Tooltip>
+                    </Col>
+                  </Row>
+                </Table.Cell>
               </Table.Row>
             );
           })}
