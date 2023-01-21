1b0931ee684c1be5f343abc7a1a798b94c32f153
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-01-21 19:25:27 +0100

    ItemCard accepts href and uses `router.push`

--- main-app/components/cards/ItemCard.tsx
+++ main-app/components/cards/ItemCard.tsx
@@ -1,19 +1,22 @@
 import { Card } from "@nextui-org/react";
+import { useRouter } from "next/router";
 import React from "react";
+import { getRouteForSingleText } from "../../util/routing/texts";
 
 interface ItemCardProps {
   name: string;
+  href: string;
   lastUpdated?: string;
-  id?: string;
 }
 
 export const ItemCard: React.FC<ItemCardProps> = ({
   name,
   lastUpdated,
-  id,
+  href,
 }) => {
+  const router = useRouter();
   return (
-    <Card isPressable onPress={() => console.log("open card")}>
+    <Card isPressable onPress={() => router.push(href)}>
       <Card.Body>
         <h4>{name}</h4>
         <p>{lastUpdated}</p>
