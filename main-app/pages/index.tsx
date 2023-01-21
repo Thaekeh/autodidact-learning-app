298bad962fad6e3f01b60a9a88da5ed32beb790e
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-01-21 23:41:36 +0100

    Remove Head from /pages/index.tsx

--- main-app/pages/index.tsx
+++ main-app/pages/index.tsx
@@ -7,11 +7,6 @@ import styled from "@emotion/styled";
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Learning Hub</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main>
         <div>Test</div>
       </main>
