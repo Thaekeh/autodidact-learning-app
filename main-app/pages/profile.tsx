67ea76e0a1e0078b42e39f54a9c4bb65b0b3e550
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-02-11 22:12:58 +0100

    LH-15 render basic profile page with name input

--- main-app/pages/profile.tsx
+++ main-app/pages/profile.tsx
@@ -1,12 +1,33 @@
 import styled from "@emotion/styled";
-import { Container, Text, theme } from "@nextui-org/react";
+import { Container, Input, theme, useInput } from "@nextui-org/react";
+import { useSession } from "next-auth/react";
 import React from "react";
 
+type fieldName = "username";
+
 export default function Profile() {
+  const { data } = useSession();
+  const { value: nameInputValue, bindings: nameInputBindings } = useInput(
+    data?.user?.name || ""
+  );
+
+  const onInputFieldBlur = (fieldName: fieldName, fieldValue: string) => {
+    // update mongo user
+    // better wait till decision about supabase is made
+  };
+
   return (
     <Container xs>
       <BackgroundDiv>
-        <Text>Text</Text>
+        <HeadingContainer>
+          <h2>Settings</h2>
+          <Input
+            label="Name"
+            value={nameInputValue}
+            onChange={nameInputBindings.onChange}
+            onBlur={() => onInputFieldBlur("username", nameInputValue)}
+          ></Input>
+        </HeadingContainer>
       </BackgroundDiv>
     </Container>
   );
@@ -16,4 +37,11 @@ const BackgroundDiv = styled.div`
   background-color: ${theme.colors.white.value};
   height: 100%;
   width: 100%;
+  padding-top: ${theme.space[18].value};
+`;
+
+const HeadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
 `;
