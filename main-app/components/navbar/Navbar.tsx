1d831b3fa62204a889f9ca73f12e9a314c70a644
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-02-11 21:18:46 +0100

    LH-22 update Navbar component

---
+++ main-app/components/navbar/Navbar.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Navbar, Button, theme } from "@nextui-org/react";
+import { useSession } from "next-auth/react";
+import NextLink from "next/link";
+import { NavbarAvatar } from "./NavbarAvatar";
+
+export const ComposedNavbar = () => {
+  const { data } = useSession();
+
+  return (
+    <Navbar
+      variant={"sticky"}
+      css={{ backgroundColor: theme.colors.cyan50.value }}
+    >
+      <Navbar.Brand>
+        <NextLink href={"/"}>
+          <h1>Learning Hub</h1>
+        </NextLink>
+      </Navbar.Brand>
+      <Navbar.Content>
+        {data?.user ? (
+          <>
+            <Navbar.Link as={"span"}>
+              <NextLink href="/app">Dashboard</NextLink>
+            </Navbar.Link>
+            <Navbar.Link as={"span"}>
+              <NextLink href="/flashcards">Flashcards</NextLink>
+            </Navbar.Link>
+            <Navbar.Link as={"span"}>
+              <NextLink href="/texts">Texts</NextLink>
+            </Navbar.Link>
+            <Navbar.Link as={"span"}>
+              <NavbarAvatar />
+            </Navbar.Link>
+          </>
+        ) : (
+          <NextLink href={"/login"}>
+            <Button>Login</Button>
+          </NextLink>
+        )}
+      </Navbar.Content>
+    </Navbar>
+  );
+};
