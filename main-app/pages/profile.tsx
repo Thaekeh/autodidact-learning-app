388ef22faa685c2a82e1b3ba4dfa37e6f35a26ea
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-02-23 00:14:51 +0100

    LH-15 get profile name for user, and update on blur

--- main-app/pages/profile.tsx
+++ main-app/pages/profile.tsx
@@ -1,70 +1,81 @@
 import styled from "@emotion/styled";
 import {
-  Card,
-  Container,
-  Input,
-  theme,
-  useInput,
-  styled as NextUIStyled,
+	Card,
+	Container,
+	Input,
+	theme,
+	useInput,
+	styled as NextUIStyled,
 } from "@nextui-org/react";
-import { useUser } from "@supabase/auth-helpers-react";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import { NextApiRequest, NextApiResponse } from "next";
-import React, { useState } from "react";
+import React from "react";
+import { Database, ProfileRow } from "../types";
 
-type fieldName = "username";
+type FieldName = "name";
 
 export async function getServerSideProps({
-  req,
-  res,
+	req,
+	res,
 }: {
-  req: NextApiRequest;
-  res: NextApiResponse;
+	req: NextApiRequest;
+	res: NextApiResponse;
 }) {
-  const user = useUser()
-  
+	const supabase = await createServerSupabaseClient<Database>({ req, res });
+	const { data: profile } = await supabase.from("profiles").select().single();
+	return { props: { profile } };
 }
 
-export default function Profile() {
-  const { value: nameInputValue, bindings: nameInputBindings } = useInput("");
+export default function Profile({ profile }: { profile: ProfileRow }) {
+	const user = useUser();
+	const supabase = useSupabaseClient();
+	const { value: nameInputValue, bindings: nameInputBindings } = useInput(
+		profile.name || ""
+	);
 
-  const onInputFieldBlur = (fieldName: fieldName, fieldValue: string) => {
-    // update mongo user
-    // better wait till decision about supabase is made
-  };
+	const onInputFieldBlur = async (fieldName: FieldName, fieldValue: string) => {
+		const updateDocument: Partial<ProfileRow> = {};
+		updateDocument[fieldName] = fieldValue;
+		await supabase
+			.from("profiles")
+			.update(updateDocument)
+			.eq("user_id", user?.id);
+	};
 
-  return (
-    <Container xs>
-      <BackgroundDiv>
-        <HeadingContainer>
-          <PaddedCard variant="bordered">
-            <h3>Settings</h3>
-            <Input
-              shadow={false}
-              label="Name"
-              value={nameInputValue}
-              onChange={nameInputBindings.onChange}
-              onBlur={() => onInputFieldBlur("username", nameInputValue)}
-            ></Input>
-          </PaddedCard>
-        </HeadingContainer>
-      </BackgroundDiv>
-    </Container>
-  );
+	return (
+		<Container xs>
+			<BackgroundDiv>
+				<HeadingContainer>
+					<PaddedCard variant="bordered">
+						<h3>Settings</h3>
+						<Input
+							shadow={false}
+							label="Name"
+							value={nameInputValue}
+							onChange={nameInputBindings.onChange}
+							onBlur={() => onInputFieldBlur("name", nameInputValue)}
+						></Input>
+					</PaddedCard>
+				</HeadingContainer>
+			</BackgroundDiv>
+		</Container>
+	);
 }
 
 const BackgroundDiv = styled.div`
-  background-color: ${theme.colors.white.value};
-  height: 100%;
-  width: 100%;
-  margin-top: ${theme.space[12].value};
+	background-color: ${theme.colors.white.value};
+	height: 100%;
+	width: 100%;
+	margin-top: ${theme.space[12].value};
 `;
 
 const HeadingContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
 `;
 
 const PaddedCard = NextUIStyled(Card, {
-  padding: theme.space[12].value,
+	padding: theme.space[12].value,
 });
