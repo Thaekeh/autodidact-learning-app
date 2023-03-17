f816cbdc8ec49d4a1c340d53bc5ffacd23b14809
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-03-17 09:58:19 +0100

    LH-49 getAllFlashcardListsNamesOnly util function

--- main-app/util/supabase/lists.ts
+++ main-app/util/supabase/lists.ts
@@ -2,18 +2,30 @@ import { SupabaseClient } from "@supabase/supabase-js";
 import { FlashcardListRow } from "../../types";
 import { flashcardListsTable } from "./tables";
 
-export const getListsForUser = async (supabase: SupabaseClient): Promise<FlashcardListRow[] | null> => {
-    const { data } = await supabase
+export const getListsForUser = async (
+	supabase: SupabaseClient
+): Promise<FlashcardListRow[] | null> => {
+	const { data } = await supabase.from(flashcardListsTable).select();
+	return data;
+};
+
+export const getListById = async (
+	supabase: SupabaseClient,
+	listId: string
+): Promise<FlashcardListRow | null> => {
+	const { data } = await supabase
 		.from(flashcardListsTable)
 		.select()
-    return data
-}
+		.eq("id", listId)
+		.single();
+	return data;
+};
 
-export const getListById = async (supabase: SupabaseClient, listId: string): Promise<FlashcardListRow | null> => {
-    const { data } = await supabase
-    .from(flashcardListsTable)
-    .select()
-    .eq("id", listId)
-    .single();
-    return data
-}
+export type FlashcardListWithNameOnly = Pick<FlashcardListRow, "name" | "id">;
+
+export const getAllFlashcardListsNamesOnly = async (
+	supabase: SupabaseClient
+): Promise<FlashcardListWithNameOnly[] | null> => {
+	const { data } = await supabase.from(flashcardListsTable).select("id, name");
+	return data;
+};
