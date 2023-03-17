8995629fda22d88df96d9f5f47e4df06029cabc5
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-03-17 09:58:05 +0100

    LH-49 enable users to select flashcardList

--- main-app/pages/texts/[...text].tsx
+++ main-app/pages/texts/[...text].tsx
@@ -1,126 +1,171 @@
 import {
-  Button,
-  Card,
-  Container,
-  Grid,
-  Row,
-  Textarea,
-  Text,
+	Container,
+	Grid,
+	Textarea,
+	Text,
+	Input,
+	Dropdown,
 } from "@nextui-org/react";
 import React, { useState } from "react";
-import { Edit2 } from "react-feather";
 import styled from "@emotion/styled";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "../../types/supabase";
 import { NextApiRequest, NextApiResponse } from "next";
 import { TextRow } from "../../types/Texts";
+import {
+	FlashcardListWithNameOnly,
+	getAllFlashcardListsNamesOnly,
+} from "../../util";
 
-export default function TextPage({ text }: { text: TextRow | null }) {
-  const [isInEditMode, setIsInEditMode] = useState(false);
+export default function TextPage({
+	text,
+	flashcardLists,
+}: {
+	text: TextRow | null;
+	flashcardLists: FlashcardListWithNameOnly[] | null;
+}) {
+	const [isInEditMode, setIsInEditMode] = useState(false);
+	const [frontOfCardValue, setFrontOfCardValue] = useState("");
+	const [backOfCardValue, setBackOfCardValue] = useState("");
+	const [selectedList, setSelectedList] = useState(
+		flashcardLists ? flashcardLists[0].id : undefined
+	);
 
-  const handleTextClick = (event: React.MouseEvent<HTMLSpanElement>) => {
-    console.log(event?.currentTarget.innerHTML);
-  };
+	const handleTextClick = (event: React.MouseEvent<HTMLSpanElement>) => {
+		console.log(event?.currentTarget.innerHTML);
+	};
 
-  const mappedText = () => {
-    if (!text?.content) return "Empty";
-    const textInArray = text.content.split(" ");
-    return textInArray.map((word) => {
-      const randomNumber = Math.floor(Math.random() * 100000);
-      return (
-        <React.Fragment key={`${word}-${randomNumber}`}>
-          <HoverableWord onClick={handleTextClick}>{word}</HoverableWord>
-          &nbsp;
-        </React.Fragment>
-      );
-    });
-  };
+	const mappedText = () => {
+		if (!text?.content) return "Empty";
+		const textInArray = text.content.split(" ");
+		return textInArray.map((word) => {
+			const randomNumber = Math.floor(Math.random() * 100000);
+			return (
+				<React.Fragment key={`${word}-${randomNumber}`}>
+					<HoverableWord onClick={handleTextClick}>{word}</HoverableWord>
+					&nbsp;
+				</React.Fragment>
+			);
+		});
+	};
 
-  const setText = (inputText: string) => {};
+	const setText = (inputText: string) => {};
 
-  return (
-    <Grid.Container
-      gap={2}
-      justify="center"
-      css={{ marginTop: `2rem`, width: `100vw`, margin: 0 }}
-    >
-      <Grid
-        xs
-        css={{
-          maxHeight: `80vh`,
-          height: `80vh`,
-        }}
-      >
-        <Card>
-          <Card.Body>
-            {isInEditMode ? (
-              <>
-                <Text h4>Edit your text</Text>
-                <Textarea
-                  animated={false}
-                  value={text?.content || ""}
-                  onChange={(event) => setText(event.target.value)}
-                  maxRows={50}
-                ></Textarea>
-              </>
-            ) : (
-              <>
-                <Text h4>Click on any word</Text>
-                <Container wrap="wrap" css={{ maxWidth: `100%` }}>
-                  <Text css={{ display: `flex`, flexWrap: `wrap` }}>
-                    {mappedText()}
-                  </Text>
-                </Container>
-              </>
-            )}
-          </Card.Body>
-          <Card.Divider />
-          <Card.Footer>
-            <Row justify="flex-end">
-              <Button size="xs" light>
-                Cancel
-              </Button>
-              <Button icon size="sm" rounded>
-                <Edit2 onClick={() => setIsInEditMode(!isInEditMode)}></Edit2>
-              </Button>
-            </Row>
-          </Card.Footer>
-        </Card>
-      </Grid>
-      <Grid xs>
-        <h1>Test</h1>
-      </Grid>
-    </Grid.Container>
-  );
+	return (
+		<Grid.Container
+			gap={2}
+			justify="center"
+			css={{ marginTop: `2rem`, width: `100vw`, margin: 0 }}
+		>
+			<Grid
+				xs={6}
+				css={{
+					maxHeight: `80vh`,
+					height: `80vh`,
+				}}
+			>
+				{isInEditMode ? (
+					<>
+						<Text h4>Edit your text</Text>
+						<Textarea
+							animated={false}
+							value={text?.content || ""}
+							onChange={(event) => setText(event.target.value)}
+							maxRows={50}
+						></Textarea>
+					</>
+				) : (
+					<>
+						<Container wrap="wrap" css={{ maxWidth: `100%` }}>
+							<Text h2>{text?.name}</Text>
+							<Text css={{ display: `flex`, flexWrap: `wrap` }}>
+								{mappedText()}
+							</Text>
+						</Container>
+					</>
+				)}
+			</Grid>
+			<Grid xs={3} direction="column">
+				<Text h3>Add card to list</Text>
+				{flashcardLists && (
+					<div>
+						<Dropdown>
+							<Dropdown.Button>
+								{
+									flashcardLists.find(
+										(flashcardList) => flashcardList.id === selectedList
+									)?.name
+								}
+							</Dropdown.Button>
+							<Dropdown.Menu
+								onAction={(key) => setSelectedList(key.toString())}
+								selectionMode="single"
+							>
+								{flashcardLists.map((flashcardList) => (
+									<Dropdown.Item key={flashcardList.id}>
+										{flashcardList.name}
+									</Dropdown.Item>
+								))}
+							</Dropdown.Menu>
+						</Dropdown>
+					</div>
+				)}
+				<div>
+					<form action="">
+						<Input
+							label="Front of card"
+							name="front"
+							required={true}
+							value={frontOfCardValue || ""}
+							onChange={(e) => setFrontOfCardValue(e.target.value)}
+						></Input>
+						<Input
+							label="Back of card"
+							name="back"
+							required={true}
+							value={backOfCardValue || ""}
+							onChange={(e) => setBackOfCardValue(e.target.value)}
+						></Input>
+					</form>
+				</div>
+			</Grid>
+		</Grid.Container>
+	);
 }
 
 export async function getServerSideProps({
-  req,
-  res,
-  params,
+	req,
+	res,
+	params,
 }: {
-  req: NextApiRequest;
-  res: NextApiResponse;
-  params: Params;
+	req: NextApiRequest;
+	res: NextApiResponse;
+	params: Params;
 }) {
-  const supabase = await createServerSupabaseClient<Database>({
-    req,
-    res,
-  });
+	const supabase = await createServerSupabaseClient<Database>({
+		req,
+		res,
+	});
 
-  const textId = params.text[0];
-  const { data: text } = await supabase.from("texts").select().eq('id', textId).single();
+	const textId = params.text[0];
+	const { data: text } = await supabase
+		.from("texts")
+		.select()
+		.eq("id", textId)
+		.single();
 
-  return { props: { text: text } };
+	const flashcardLists = await getAllFlashcardListsNamesOnly(supabase);
+
+	return { props: { text: text, flashcardLists } };
 }
 
 const HoverableWord = styled.span`
-  padding: 1px;
-  box-sizing: border-box;
-  :hover {
-    background-color: #8686ff;
-    cursor: pointer;
-    border-radius: 3px;
-  }
+	padding: 1px;
+	box-sizing: border-box;
+	:hover {
+		background-color: #8686ff;
+		cursor: pointer;
+		border-radius: 3px;
+	}
 `;
