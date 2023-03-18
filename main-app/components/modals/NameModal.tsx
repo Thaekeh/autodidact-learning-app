ed9abcd2d4e068d59d94b9bffd2c0942f2cba998
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-03-18 11:27:46 +0100

    LH-52 use spaces

--- main-app/components/modals/NameModal.tsx
+++ main-app/components/modals/NameModal.tsx
@@ -2,40 +2,40 @@ import { Button, Input, Modal, useInput, Text } from "@nextui-org/react";
 import React from "react";
 
 interface NameModalProps {
-	title: string;
-	isOpen: boolean;
-	onConfirm: (name: string) => void;
-	onCancel: () => void;
+  title: string;
+  isOpen: boolean;
+  onConfirm: (name: string) => void;
+  onCancel: () => void;
 }
 
 export const NameModal: React.FC<NameModalProps> = ({
-	title,
-	isOpen,
-	onConfirm,
-	onCancel,
+  title,
+  isOpen,
+  onConfirm,
+  onCancel,
 }) => {
-	const { value, bindings } = useInput("");
+  const { value, bindings } = useInput("");
 
-	return (
-		<Modal closeButton open={isOpen} onClose={onCancel}>
-			<Modal.Header>
-				<Text size={20} weight={"bold"}>
-					{title}
-				</Text>
-			</Modal.Header>
-			<Modal.Body>
-				<Input
-					value={value}
-					label="Name"
-					onChange={bindings.onChange}
-					placeholder="Name"
-				/>
-			</Modal.Body>
-			<Modal.Footer>
-				<Button auto onPress={() => onConfirm(value)}>
-					Create
-				</Button>
-			</Modal.Footer>
-		</Modal>
-	);
+  return (
+    <Modal closeButton open={isOpen} onClose={onCancel}>
+      <Modal.Header>
+        <Text size={20} weight={"bold"}>
+          {title}
+        </Text>
+      </Modal.Header>
+      <Modal.Body>
+        <Input
+          value={value}
+          label="Name"
+          onChange={bindings.onChange}
+          placeholder="Name"
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button auto onPress={() => onConfirm(value)}>
+          Create
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 };
