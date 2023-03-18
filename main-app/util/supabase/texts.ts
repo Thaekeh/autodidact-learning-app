10bbd8dd053105281271b11a41608d04faa6008c
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-03-18 17:28:02 +0100

    LH-59 deleteText and getTexts supabase utils

--- main-app/util/supabase/texts.ts
+++ main-app/util/supabase/texts.ts
@@ -1,23 +1,38 @@
 import { SupabaseClient } from "@supabase/supabase-js";
 import { TextRow } from "../../types";
+import { textsTable } from "./tables";
 
 export const createNewText = async (
-	supabaseClient: SupabaseClient,
-	name: string
+  supabaseClient: SupabaseClient,
+  name: string
 ): Promise<TextRow> => {
-	const user = await supabaseClient.auth.getSession();
-	const { data } = await supabaseClient
-		.from("texts")
-		.insert({ name, user_id: user.data.session?.user.id })
-		.select()
-		.single();
-	return data;
+  const user = await supabaseClient.auth.getSession();
+  const { data } = await supabaseClient
+    .from("texts")
+    .insert({ name, user_id: user.data.session?.user.id })
+    .select()
+    .single();
+  return data;
 };
 
 export const saveTextContent = async (
-	supabaseClient: SupabaseClient,
-	textId: string,
-	content: string
+  supabaseClient: SupabaseClient,
+  textId: string,
+  content: string
 ) => {
-	await supabaseClient.from("texts").update({ content }).eq("id", textId);
+  await supabaseClient.from("texts").update({ content }).eq("id", textId);
+};
+
+export const deleteText = async (
+  supabaseClient: SupabaseClient,
+  textId: string
+) => {
+  await supabaseClient.from(textsTable).delete().eq("id", textId);
+};
+
+export const getTexts = async (
+  supabaseClient: SupabaseClient
+): Promise<TextRow[] | null> => {
+  const { data } = await supabaseClient.from(textsTable).select();
+  return data;
 };
