734e259314a97181fa7d46a88aa555795b48e142
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-03-18 11:11:37 +0100

    LH-52 add more table consts

--- main-app/util/supabase/tables.ts
+++ main-app/util/supabase/tables.ts
@@ -1,2 +1,4 @@
-export const flashcardListsTable = 'flashcardLists';
-export const flashcardsTable = 'flashcards';
+export const flashcardListsTable = "flashcardLists";
+export const flashcardsTable = "flashcards";
+export const profilesTable = "profiles";
+export const textsTable = "texts";
