d057672d0023b451c7899f0ec1ed170f7bec6bfc
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-03-18 18:42:50 +0100

    LH-59 useConfirm hook

---
+++ main-app/hooks/useConfirm.tsx
@@ -0,0 +1,41 @@
+import { useContext, useEffect, useState } from "react";
+import { ConfirmContext } from "../providers/ConfirmContextProvider";
+
+export const useConfirm = () => {
+  const [confirm, setConfirm] = useContext(ConfirmContext);
+  const [needsCleanup] = useState(false);
+
+  const isConfirmed = (prompt: string) => {
+    const promise = new Promise((resolve, reject) => {
+      setConfirm({
+        prompt,
+        isOpen: true,
+        proceed: resolve,
+        cancel: reject,
+      });
+    });
+    return promise.then(
+      () => {
+        setConfirm({ ...confirm, isOpen: false });
+        return true;
+      },
+      () => {
+        setConfirm({ ...confirm, isOpen: false });
+        return false;
+      }
+    );
+  };
+
+  useEffect(() => {
+    return () => {
+      if (confirm.cancel && needsCleanup) {
+        confirm.cancel(undefined);
+      }
+    };
+  }, [confirm, needsCleanup]);
+
+  return {
+    ...confirm,
+    isConfirmed,
+  };
+};
