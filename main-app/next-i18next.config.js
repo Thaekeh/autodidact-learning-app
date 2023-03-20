b46d8aa37cb3f87b829e63a68042efeb569ecd96
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-03-20 20:50:20 +0100

    LH-10 more i18next config

--- main-app/next-i18next.config.js
+++ main-app/next-i18next.config.js
@@ -3,4 +3,10 @@ module.exports = {
     defaultLocale: "en",
     locales: ["en"],
   },
+  reloadOnPrerender: process.env.NODE_ENV === "development",
+  debug: process.env.NODE_ENV === "development",
+  localePath:
+    typeof window === "undefined"
+      ? require("path").resolve("./public/locales")
+      : "/locales",
 };
