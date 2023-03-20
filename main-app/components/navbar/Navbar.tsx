e066c1b028d161f25dfd151ef7dbb6b2a7ba2a22
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-03-20 21:25:52 +0100

    LH-53 implement basic middleware

--- main-app/components/navbar/Navbar.tsx
+++ main-app/components/navbar/Navbar.tsx
@@ -3,51 +3,51 @@ import { Navbar, Button } from "@nextui-org/react";
 import NextLink from "next/link";
 import { NavbarAvatar } from "./NavbarAvatar";
 import { useUser } from "@supabase/auth-helpers-react";
-import { getRouteForAllFlashcardLists } from "../../util";
+import { getRouteForAllFlashcardLists, getRouteForAllTexts } from "../../util";
 
 export const ComposedNavbar = () => {
-	const user = useUser();
+  const user = useUser();
 
-	return (
-		<Navbar variant={"sticky"}>
-			<Navbar.Brand>
-				<NextLink href={user ? "/app" : "/"}>
-					<h1>Learning Hub</h1>
-				</NextLink>
-			</Navbar.Brand>
-			<Navbar.Content>
-				<Navbar.Content>
-					{user ? (
-						<>
-							<Navbar.Link as={"span"}>
-								<NextLink href="/app">Dashboard</NextLink>
-							</Navbar.Link>
-							<Navbar.Link as={"span"}>
-								<NextLink href={getRouteForAllFlashcardLists()}>
-									Flashcards
-								</NextLink>
-							</Navbar.Link>
-							<Navbar.Link as={"span"}>
-								<NextLink href="/texts">Texts</NextLink>
-							</Navbar.Link>
-						</>
-					) : (
-						<NextLink href={"/login"}>
-							<Button size={"sm"}>Login</Button>
-						</NextLink>
-					)}
-				</Navbar.Content>
-				<Navbar.Content
-					css={{
-						"@xs": {
-							w: "12%",
-							jc: "flex-end",
-						},
-					}}
-				>
-					{user && <NavbarAvatar />}
-				</Navbar.Content>
-			</Navbar.Content>
-		</Navbar>
-	);
+  return (
+    <Navbar variant={"sticky"}>
+      <Navbar.Brand>
+        <NextLink href={user ? "/app" : "/"}>
+          <h1>Learning Hub</h1>
+        </NextLink>
+      </Navbar.Brand>
+      <Navbar.Content>
+        <Navbar.Content>
+          {user ? (
+            <>
+              <Navbar.Link as={"span"}>
+                <NextLink href="/app">Dashboard</NextLink>
+              </Navbar.Link>
+              <Navbar.Link as={"span"}>
+                <NextLink href={getRouteForAllFlashcardLists()}>
+                  Flashcards
+                </NextLink>
+              </Navbar.Link>
+              <Navbar.Link as={"span"}>
+                <NextLink href={getRouteForAllTexts()}>Texts</NextLink>
+              </Navbar.Link>
+            </>
+          ) : (
+            <NextLink href={"/login"}>
+              <Button size={"sm"}>Login</Button>
+            </NextLink>
+          )}
+        </Navbar.Content>
+        <Navbar.Content
+          css={{
+            "@xs": {
+              w: "12%",
+              jc: "flex-end",
+            },
+          }}
+        >
+          {user && <NavbarAvatar />}
+        </Navbar.Content>
+      </Navbar.Content>
+    </Navbar>
+  );
 };
