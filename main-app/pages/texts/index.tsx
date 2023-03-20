e4f04543611f876f3c16badab74d485c2f198ee8
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-03-20 20:57:02 +0100

    LH-62 use FullTable on texts page

--- main-app/pages/texts/index.tsx
+++ main-app/pages/texts/index.tsx
@@ -10,57 +10,26 @@ import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "../../types/supabase";
 import { getLocale } from "../../util/translation/getLocale";
 import { useTranslation } from "next-i18next";
+import { FullTable } from "../../components/table/FullTable";
 
 export default function Texts({ texts }: { texts: TextRow[] }) {
   const { t } = useTranslation();
 
+  const handleOpenCallback = (id: string) => {
+    console.log(id);
+  };
+
+  const handleDeleteCallback = (id: string) => {
+    console.log(id);
+  };
+
   return (
     <Container css={{ marginTop: "$18" }}>
-      <Table>
-        <Table.Header>
-          <Table.Column>{t("name")}</Table.Column>
-          {/* <Table.Column>Last opened</Table.Column> */}
-          <Table.Column> </Table.Column>
-        </Table.Header>
-        <Table.Body>
-          {texts.map((text) => {
-            return (
-              <Table.Row key={text.id}>
-                <Table.Cell>{text.name}</Table.Cell>
-                {/* <Table.Cell>{text.updatedAt}</Table.Cell> */}
-
-                <Table.Cell>
-                  <Row justify="center" align="center">
-                    <Col css={{ d: "flex" }}>
-                      <Tooltip content="Open text">
-                        <Link href={getRouteForSingleText(text.id)}>
-                          <IconButton>
-                            <Eye />
-                          </IconButton>
-                        </Link>
-                      </Tooltip>
-                    </Col>
-                    {/* <Col css={{ d: "flex" }}>
-											<Tooltip content="Rename text">
-												<IconButton>
-													<Edit2 />
-												</IconButton>
-											</Tooltip>
-										</Col> */}
-                    <Col css={{ d: "flex" }}>
-                      <Tooltip content="Delete text">
-                        <IconButton>
-                          <Trash color={red.red600} />
-                        </IconButton>
-                      </Tooltip>
-                    </Col>
-                  </Row>
-                </Table.Cell>
-              </Table.Row>
-            );
-          })}
-        </Table.Body>
-      </Table>
+      <FullTable
+        items={texts}
+        openCallBack={handleOpenCallback}
+        deleteCallback={handleDeleteCallback}
+      ></FullTable>
     </Container>
   );
 }
