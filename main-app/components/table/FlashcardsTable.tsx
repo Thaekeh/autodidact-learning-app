9fabd308f152be90cd5c4ae22d271d8b5a3e03b4
Thaeke Hekkenberg <thaekeh@gmail.com>
2023-05-21 14:13:16 +0200

    LH-64/ show learning phases in table

--- main-app/components/table/FlashcardsTable.tsx
+++ main-app/components/table/FlashcardsTable.tsx
@@ -1,8 +1,17 @@
-import { Row, Spacer, Table, theme, Tooltip } from "@nextui-org/react";
-import { Check, Edit, Trash2 } from "react-feather";
+import { Row, Spacer, Table, Tooltip } from "@nextui-org/react";
+import { Edit, Trash2 } from "react-feather";
 import { IconButton } from "components/buttons/IconButton";
-import { FlashcardRow } from "types";
+import { FlashcardRow, ProfileRow } from "types";
 import { DateTime } from "luxon";
+import {
+  defaultFirstPhaseInterval,
+  defaultSecondPhaseInterval,
+  FlashcardPhase,
+  mapFlashcardIntervalToLearningPhase,
+} from "utils/mapping/flashcards";
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
+import { getProfileByUserId } from "utils/supabase/profiles";
+import { useEffect, useState } from "react";
 
 interface Props {
   flashcards: FlashcardRow[];
@@ -15,13 +24,26 @@ export const FlashcardsTable: React.FC<Props> = ({
   editCallback,
   deleteCallback,
 }) => {
+  const [profile, setProfile] = useState<ProfileRow | null>(null);
+  const supabase = useSupabaseClient();
+  const user = useUser();
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      if (!user) return;
+      const profile = await getProfileByUserId(supabase, user.id);
+      setProfile(profile);
+    };
+    fetchProfile();
+  }, []);
+
   return (
     <Table>
       <Table.Header>
         <Table.Column>Front</Table.Column>
         <Table.Column>Back</Table.Column>
         <Table.Column>Next practice date</Table.Column>
-        <Table.Column>Learned</Table.Column>
+        <Table.Column>Learning Phase</Table.Column>
         <Table.Column>Actions</Table.Column>
       </Table.Header>
       <Table.Body>
@@ -31,8 +53,12 @@ export const FlashcardsTable: React.FC<Props> = ({
               card.next_practice_date &&
               DateTime.fromISO(card.next_practice_date).toLocaleString();
 
-            const isLearnedIcon = card.interval > 21 && (
-              <Check color={theme.colors.green700.value} />
+            const learningPhaseForCard = mapFlashcardIntervalToLearningPhase(
+              card.interval,
+              profile?.first_learning_phase_interval ||
+                defaultFirstPhaseInterval,
+              profile?.second_learning_phase_interval ||
+                defaultSecondPhaseInterval
             );
 
             return (
@@ -40,7 +66,7 @@ export const FlashcardsTable: React.FC<Props> = ({
                 <Table.Cell>{card.frontText || "Empty"}</Table.Cell>
                 <Table.Cell>{card.backText || "Empty"}</Table.Cell>
                 <Table.Cell>{formattedNextPracticeDate}</Table.Cell>
-                <Table.Cell>{isLearnedIcon}</Table.Cell>
+                <Table.Cell>{learningPhaseForCard}</Table.Cell>
                 <Table.Cell>
                   <Row>
                     <Tooltip content="Edit">
